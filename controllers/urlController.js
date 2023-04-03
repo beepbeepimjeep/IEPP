779fcbe46916ae91364c5a70083dc52420f71080
beepbeepimjeep <42129550+beepbeepimjeep@users.noreply.github.com>
2023-04-04 00:08:07 +1000

    connect mysql

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,8 +1,18 @@
 const axios = require('axios');
+const mysql = require('mysql');
+
 require('dotenv').config();
 
+const connection = mysql.createConnection({
+    host: '34.129.14.82',
+    port: 3306,
+    user: 'root',
+    password: '012ebecbd6b870d3',
+    database: '5120finalproject',
+    connectionTimeout: 3000
+});
 
-const urlSearch = async (req,res)=>{
+const urlSearch = async (req, res) => {
     var domain = req.params.url;
 
     axios.get(`https://webrisk.googleapis.com/v1/uris:search?threatTypes=MALWARE&uri=${domain}&key=${process.env["GOOGLE_LOOKUP_API"]}`)
@@ -14,7 +24,24 @@ const urlSearch = async (req,res)=>{
             // Handle any errors that occurred during the request
             console.log(error);
         });
-    res.render('')
+
+    connection.connect((err) => {
+        if (err) throw err;
+        console.log('Connected to MySQL server!');
+
+        connection.query('SELECT * FROM riskType_Article LIMIT 5', (err, results) => {
+            if (err) throw err;
+            console.log(results);
+        });
+
+        // Close the connection when you're done with it
+        connection.end((err) => {
+            if (err) throw err;
+            console.log('Connection closed!');
+        });
+    });
+
+    res.render('');
 }
 
 module.exports = {
