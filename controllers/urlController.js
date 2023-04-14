eb02437aba962bfc97f1d3b1794939008565a215
beepbeepimjeep <42129550+beepbeepimjeep@users.noreply.github.com>
2023-04-15 00:56:15 +1000

    update to fuzzy search

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -15,45 +15,69 @@ const urlSearch = async (domain) => {
     });
     const pattern1 = "http://";
     const pattern2 = "https://";
-    console.log("line 18 domain = " +domain);
-    domain = domain.replace(pattern1, "");
-    domain = domain.replace(pattern2, "");
-
+    //domain = domain.replace(pattern1, "");
+    //domain = domain.replace(pattern2, "");
+    const regex1 = RegExp(pattern1);
+    const regex2 = RegExp(pattern2);
     console.log("domain after replace: "+domain);
-
-    let sentData;
-    try {
-        const results = await Promise.all([
-            new Promise((resolve, reject) => {
-                connection.query(`SELECT * FROM collectionURL WHERE urlLink = '${domain}' `, (err, results) => {
-                    if (err) reject(err);
-                    else resolve(results);
-                });
-            }),
-            new Promise((resolve, reject) => {
-                connection.query(`SELECT * FROM maliciousURL WHERE urlLink = '${domain}' `, (err, results) => {
-                    if (err) reject(err);
-                    else resolve(results);
-                });
-            }),
-            axios.get(`https://webrisk.googleapis.com/v1/uris:search?threatTypes=MALWARE&uri=${domain}&key=${process.env["GOOGLE_LOOKUP_API"]}`)
-                .then(response => {
-                    return response.data;
-                })
-                .catch(error => {
-                    console.log(error);
-                    return [];
-                })
-        ]);
-        connection.end((err)=>{
-            if(err) throw err;
-            console.log("Line 52, connection pool close")
-        })
-        console.log(results);
-        return results;
-    } catch (error) {
-        console.log(error);
-        throw new Error('Internal server error');
+    if((regex1.test(domain)&&domain.length<17)||(regex2.test(domain)&&domain.length<18)||(domain.length<10)){
+        try{
+            const results = await Promise.all([
+                [],
+                [],
+                axios.get(`https://webrisk.googleapis.com/v1/uris:search?threatTypes=MALWARE&uri=${domain}&key=${process.env["GOOGLE_LOOKUP_API"]}`)
+                    .then(response => {
+                        return response.data;
+                    })
+                    .catch(error => {
+                        console.log(error);
+                        return [];
+                    })
+            ]);
+            connection.end((err)=>{
+                if(err) throw err;
+                console.log("Line 40, connection pool close")
+            })
+            console.log(results);
+            return results;
+        }catch (error) {
+            console.log(error);
+            throw new Error('Internal server error');
+        }
+    }else{
+        try {
+            const results = await Promise.all([
+                new Promise((resolve, reject) => {
+                    connection.query(`SELECT * FROM collectionURL WHERE urlLink LIKE '%${domain}%' `, (err, results) => {
+                        if (err) reject(err);
+                        else resolve(results);
+                    });
+                }),
+                new Promise((resolve, reject) => {
+                    connection.query(`SELECT * FROM maliciousURL WHERE urlLink LIKE '%${domain}%' `, (err, results) => {
+                        if (err) reject(err);
+                        else resolve(results);
+                    });
+                }),
+                axios.get(`https://webrisk.googleapis.com/v1/uris:search?threatTypes=MALWARE&uri=${domain}&key=${process.env["GOOGLE_LOOKUP_API"]}`)
+                    .then(response => {
+                        return response.data;
+                    })
+                    .catch(error => {
+                        console.log(error);
+                        return [];
+                    })
+            ]);
+            connection.end((err)=>{
+                if(err) throw err;
+                console.log("Line 74, connection pool close")
+            })
+            console.log(results);
+            return results;
+        } catch (error) {
+            console.log(error);
+            throw new Error('Internal server error');
+        }
     }
 };
 
