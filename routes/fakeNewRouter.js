4a4b631fb3370ee1a26bd4ac808f2ffd4f05bfd8
beepbeepimjeep <42129550+beepbeepimjeep@users.noreply.github.com>
2023-04-23 21:54:11 +1000

    update
    update

--- routes/fakeNewRouter.js
+++ routes/fakeNewRouter.js
@@ -7,23 +7,46 @@ fakeNewRouter.get("/", async (req,res)=>{
         const claim = req.query.claim;
         const result = await fakeNewController.fakeNewChecker(claim);
         const claims = result.claims;
-
-        for (let i = 0; i < claims.length; i++) {
-            const claim = claims[i];
-            console.log("Claim text:", claim.text);
-            console.log("Claimant:", claim.claimant);
-            console.log("Claim date:", claim.claimDate);
-            const claimReviews = claim.claimReview;
-            for (let j = 0; j < claimReviews.length; j++) {
-                const claimReview = claimReviews[j];
-                console.log("Claim review source:", claimReview.publisher?.name);
-                console.log("Claim review URL:", claimReview.url);
-                console.log("Claim review title:", claimReview.title);
-                console.log("Claim review verdict:", claimReview.textualRating);
+        const resultArray = [];
+        if(claims == null){
+            console.log("Empty");
+            const emptyResult = "Your claim did not match any result";
+            res.json({emptyResult});
+        }else{
+            var length;
+            if(claims.length<4){
+                length = claims.length;
+            }else{
+                length = 3;
             }
+            for (let i = 0; i < length; i++) {
+                const claim = claims[i];
+                console.log("Claim number: "+i);
+                console.log("Claim text:", claim.text);
+                //console.log("Claimant:", claim.claimant);
+                //console.log("Claim date:", claim.claimDate);
+                const claimReviews = claim.claimReview;
+                for (let j = 0; j < claimReviews.length; j++) {
+                    const claimReview = claimReviews[j];
+                    //console.log("Claim review source:", claimReview.publisher?.name);
+                    console.log("Claim review URL:", claimReview.url);
+                    //console.log("Claim review title:", claimReview.title);
+                    console.log("Claim review verdict:", claimReview.textualRating);
+
+
+                    const claimResult = {
+                        claim: claim.text,
+                        result: claimReview.textualRating,
+                        url: claimReview.url
+                    };
+
+                    resultArray.push(claimResult);
+                }
+                console.log('-------------------------------------');
+            }
+            res.json({ resultArray });
         }
 
-        res.json({ claims });
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
