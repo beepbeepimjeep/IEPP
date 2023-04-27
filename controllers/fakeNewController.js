18707201ad1e68695e3ad603ea2731ad3683e90e
beepbeepimjeep <42129550+beepbeepimjeep@users.noreply.github.com>
2023-04-27 21:15:30 +1000

    news claim check db

--- controllers/fakeNewController.js
+++ controllers/fakeNewController.js
@@ -1,19 +1,43 @@
 const axios = require('axios');
 require('dotenv').config();
-const flatted = require('flatted');
+const mysql = require('mysql');
+
 const fakeNewChecker = async (news)=>{
     const claim = news;
     const apiKey = process.env["GOOGLE_LOOKUP_API"];
+    const connection = mysql.createConnection({
+        host: '34.129.14.82',
+        port: 3306,
+        user: 'root',
+        password: '012ebecbd6b870d3',
+        database: '5120finalproject'
+    });
+    // check db first, if the result is null, then check api
+    try {
+        const resultDb = await new Promise((resolve, reject) => {
+                connection.query(`SELECT * FROM FackNews WHERE article_title LIKE '%${claim}%' LIMIT 2`, (err, results) => {
+                    if (err) reject(err);
+                    else resolve(results);
+                });
+            })
+        if (resultDb.length> 0) {
+            return resultDb;
+        } else {
+            try {
+                const response = await axios.get(`https://factchecktools.googleapis.com/v1alpha1/claims:search?key=${apiKey}&query=${claim}`);
+                return response.data;
+            } catch (error) {
+                console.log(error);
+                return [];
+            }
+        }
+    } catch (error) {
+        console.log("Error: " + error);
+        return null;
+    }
+
 
     /*try{
-        const response = await axios.get(`https://factchecktools.googleapis.com/v1alpha1/claims:search?key=${apiKey}&query=${claim}`);
-        const data = flatted.stringify(response);
-        return data;
-    }catch (error){
-        console.log(error);
-        return null;
-    }*/
-    try{
         const data = axios.get(`https://factchecktools.googleapis.com/v1alpha1/claims:search?key=${apiKey}&query=${claim}`)
             .then(response => {
                 //console.log(response.data)
@@ -27,7 +51,7 @@ const fakeNewChecker = async (news)=>{
     }catch (error){
         console.log(error)
         return null;
-    }
+    }*/
 
 }
 
--- routes/fakeNewRouter.js
+++ routes/fakeNewRouter.js
@@ -6,47 +6,58 @@ fakeNewRouter.get("/", async (req,res)=>{
     try {
         const claim = req.query.claim;
         const result = await fakeNewController.fakeNewChecker(claim);
-        const claims = result.claims;
         const resultArray = [];
-        if(claims == null){
-            console.log("Empty");
-            const emptyResult = "Your claim did not match any result";
-            res.json({emptyResult});
+        if(typeof result === 'object' && Array.isArray(result)){
+            for(let i =0; i<result.length; i++){
+
+                const claimResult = {
+                    claim: result[i].article_title,
+                    result: 'False',
+                    url: ''
+                };
+                resultArray.push(claimResult);
+
+            }
         }else{
-            var length;
-            if(claims.length<4){
-                length = claims.length;
+            const claims = result.claims;
+
+            if(claims == null){
+                console.log("Empty");
+                const emptyResult = "Your claim did not match any result";
+                res.json({emptyResult});
             }else{
-                length = 3;
-            }
-            for (let i = 0; i < length; i++) {
-                const claim = claims[i];
-                console.log("Claim number: "+i);
-                console.log("Claim text:", claim.text);
-                //console.log("Claimant:", claim.claimant);
-                //console.log("Claim date:", claim.claimDate);
-                const claimReviews = claim.claimReview;
-                for (let j = 0; j < claimReviews.length; j++) {
-                    const claimReview = claimReviews[j];
-                    //console.log("Claim review source:", claimReview.publisher?.name);
-                    console.log("Claim review URL:", claimReview.url);
-                    //console.log("Claim review title:", claimReview.title);
-                    console.log("Claim review verdict:", claimReview.textualRating);
+                let length = claims.length<4?claims.length:3;
 
+                for (let i = 0; i < length; i++) {
+                    const claim = claims[i];
+                    //console.log("Claim number: "+i);
+                    //console.log("Claim text:", claim.text);
+                    //console.log("Claimant:", claim.claimant);
+                    //console.log("Claim date:", claim.claimDate);
+                    const claimReviews = claim.claimReview;
+                    for (let j = 0; j < claimReviews.length; j++) {
+                        const claimReview = claimReviews[j];
+                        //console.log("Claim review source:", claimReview.publisher?.name);
+                        //console.log("Claim review URL:", claimReview.url);
+                        //console.log("Claim review title:", claimReview.title);
+                        //console.log("Claim review verdict:", claimReview.textualRating);
 
-                    const claimResult = {
-                        claim: claim.text,
-                        result: claimReview.textualRating,
-                        url: claimReview.url
-                    };
+                        // create a struct and insert it into the result array
+                        const claimResult = {
+                            claim: claim.text,
+                            result: claimReview.textualRating,
+                            url: claimReview.url
+                        };
 
-                    resultArray.push(claimResult);
+                        resultArray.push(claimResult);
+                    }
+                    //console.log('-------------------------------------');
                 }
-                console.log('-------------------------------------');
-            }
-            res.json({ resultArray });
-        }
 
+            }
+        }
+        console.log(resultArray);
+        res.json({ resultArray });
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
